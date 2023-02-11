caf99393e5c7ad8fc981b72dcd725ba2b269511c
sofia604 <51239054+sofia604@users.noreply.github.com>
2023-02-11 12:26:10 -0500

    Update weather.js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,9 +1,23 @@
 
+import { weather_data } from './data.js';
 
 
 let loadDayForecastData = () => {
-	
-}
+    let keys = Object.keys(weather_data[0]);
+    for (let j = 1; j <  8; j++){
+        let show = document.getElementById(keys[j]);
+        show.textContent = weather_data[0][keys[j]];
+    };
+
+    let card_content = weather_data[0].forecast_today;
+    for(let value in card_content){
+        let claves = Object.keys(card_content[value]);
+        for(let i = 1; i < claves.length; i++){
+            let elem = document.getElementById(card_content[value].lapse+"_"+claves[i]);
+            elem.textContent = card_content[value][claves[i]];
+        };
+    };
+};
 
 let loadWeekForecastData = () => {
 	
