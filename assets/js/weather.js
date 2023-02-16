3330b12db49ad2cdff03255783137d474d723282
sofia604 <sespana@espol.edu.ec>
2023-02-15 23:21:05 -0500

    part I

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -20,7 +20,15 @@ let loadDayForecastData = () => {
 };
 
 let loadWeekForecastData = () => {
-	
+	let week_content = weather_data[0].forecast_week[0];
+    let element_data = document.getElementsByClassName("d-flex flex-column");
+    let mess_text = `<h6 class="mb-1 text-dark font-weight-bold text-sm">${week_content.text}</h6><span class="text-xs">${week_content.date}</span>`;
+    element_data[0].innerHTML=mess_text;
+    let element_temp = document.getElementsByClassName("d-flex align-items-center");
+    let mess_temp = `<span class="font-weight-bold text-dark mx-2">${week_content.temperature.max}</span> |  <span class="text-dark mx-2">${week_content.temperature.min}</span>
+    <div class="ms-4"><i class="material-icons fs-2 me-1 rainy">${week_content.icon}</i></div>`;
+    element_temp[0].innerHTML=mess_temp;
+
 	
 }
 
